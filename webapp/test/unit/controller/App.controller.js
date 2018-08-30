be2e115edf4718e50c848a67f9ff130a992f3d9c
Michael Graf <mi.graf@sap.com>
2018-08-30 07:31:07 +0200

    Fixed eslint warning in unit test file

--- webapp/test/unit/controller/App.controller.js
+++ webapp/test/unit/controller/App.controller.js
@@ -2,13 +2,13 @@
 
 sap.ui.define([
 	"sap/ui/demo/basicTemplate/controller/App.controller"
-], function(oController) {
+], function(Controller) {
 	"use strict";
 
 	QUnit.module("App Controller");
 
 	QUnit.test("I should test the app controller", function (assert) {
-		var oAppController = new oController();
+		var oAppController = new Controller();
 		oAppController.onInit();
 		assert.ok(oAppController);
 	});
